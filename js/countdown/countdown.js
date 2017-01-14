29f3d61eaf30857533a82a6eb7471f3166c65e91
Brayan Gallardo <brgallar@uci.edu>
2017-01-14 02:01:00 -0800

    initial commit

---
+++ js/countdown/countdown.js
@@ -0,0 +1,12 @@
+var deadline = '0:0:0';
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'seconds': seconds
+  };
+}
