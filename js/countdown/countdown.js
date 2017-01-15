9b98b6ad1d45926dd3989bc9d0e3b804e0a989ce
ruftyguhfda <ruftyguhfda@users.noreply.github.com>
2017-01-14 23:22:28 -0800

    added recursion and deleted redundant code

--- js/countdown/countdown.js
+++ js/countdown/countdown.js
@@ -1,67 +1,32 @@
-function getTimeRemaining(endtime) {
+function secondsLeft(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  
   return {
     'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
     'seconds': seconds
   };
 }
 
-function initializeClock(id, endtime) {
+function initializeClock(id) {
   var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+  var deadline = new Date(Date.parse(new Date()) + 30 * 1000);
+  
+  var secondsDisplay = clock.querySelector('.seconds');
 
   function updateClock() {
-    var t = getTimeRemaining(endtime);
+    var t = secondsLeft(deadline);
 
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    secondsDisplay.innerHTML = ('0' + t.seconds).slice(-2);
 
-    if (t.total <= 0) {
+    if (t.total <= -1) {
       clearInterval(timeinterval);
-    }
+      initializeClock(id);                    // utilize recursion so that immediately after 0 seconds,
+    }                                         // it restarts the clock
   }
 
   updateClock();
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 30 * 1000);
-
-initializeClock('clockdiv', deadline);
-
-// Below is a new version of the countdown clock (W/O fancy graphics). 
-// It has basic functionality -- it will countdown from 30s, and will
-// restart immediately when reached to 0 seconds.
-
-/*
-function startTimer(duration, display) {
-    var timer = duration, seconds 
-    setInterval(function () {
-
-        seconds = parseInt(timer % 60, 10);
-
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-window.onload = function () {
-    var thirtySec = 30,
-        display = document.querySelector('#time'); // you can change the '#time' to whatever the html doc says
-                                                   // when it comes to displaying the time.
-    startTimer(thirtySec, display);
-}; *\
+initializeClock('clockdiv');
